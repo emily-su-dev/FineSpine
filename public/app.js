856c346277c530515e8da71a400b27039db0bbae
Emily <emilysucanada@gmail.com>
2025-01-18 18:11:34 -0500

    auth0 login redirect

--- public/app.js
+++ public/app.js
@@ -1,3 +1,54 @@
+// Create Auth0 client 
+auth0.createAuth0Client({
+  domain: "dev-4og5oz66ye0htgcp.us.auth0.com",
+  clientId: "zhHeAAONqHM6lzrgcp4u7oYp5T0UGWQo",
+  authorizationParams: {
+    redirect_uri: window.location.origin
+  }
+}).then(async (auth0Client) => {
+  // Assumes a button with id "login" in the DOM
+  const loginButton = document.getElementById("login");
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth0Client.loginWithRedirect();
+  });
+
+  if (location.search.includes("state=") && 
+      (location.search.includes("code=") || 
+      location.search.includes("error="))) {
+    await auth0Client.handleRedirectCallback();
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  // Assumes a button with id "logout" in the DOM
+  const logoutButton = document.getElementById("logout");
+
+  logoutButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth0Client.logout();
+  });
+
+  const isAuthenticated = await auth0Client.isAuthenticated();
+  const userProfile = await auth0Client.getUser();
+
+  // Assumes an element with id "profile" in the DOM
+  const profileElement = document.getElementById("profile");
+
+  if (isAuthenticated) {
+    profileElement.style.display = "block";
+    profileElement.innerHTML = `
+            <p>${userProfile.name}</p>
+            <img src="${userProfile.picture}" />
+          `;
+  } else {
+    profileElement.style.display = "none";
+  }
+});
+
+
+// Video display and posture checking page 
+
 // No imports needed; modules are loaded globally
 const video = document.createElement('video');
 video.setAttribute('autoplay', '');
